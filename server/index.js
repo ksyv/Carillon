91fcb502d88959f714406d8824f268392cff387f
ksyv <ksyviais@gmail.com>
2026-02-19 11:09:18 +0000

    modif de deploiement

--- server/index.js
+++ server/index.js
@@ -147,6 +147,14 @@ app.get('/api/report', auth(['admin']), async (req, res) => {
     
     res.json(report);
 });
+// --- DEPLOIEMENT PRODUCTION ---
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
