da91bb6d7823d4f1506227727b1eed9eb2b9e39e
ksyv <ksyviais@gmail.com>
2026-06-22 15:44:25 +0200

    fix mauvais import

--- client/src/components/FamilyPortal/TabFactures.jsx
+++ client/src/components/FamilyPortal/TabFactures.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../api';
+import api from '../../api';
 import { FileText, Download, AlertCircle, Calendar } from 'lucide-react';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
